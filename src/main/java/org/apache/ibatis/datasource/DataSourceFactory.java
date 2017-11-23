7faf6d907fe2f327fe428766065b4e139acfebf6
lijinlong02 <lijinlong02@baidu.com>
2017-11-23 16:13:23 +0800

    first commit

---
+++ src/main/java/org/apache/ibatis/datasource/DataSourceFactory.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright ${license.git.copyrightYears} the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ibatis.datasource;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+/**
+ * 数据源接口（默认实现3种数据源：PooledDataSourceFactory，UnpooledDataSourceFactory， JndiDataSourceFactory）
+ * 如需自定义数据源，需要实现该接口
+ *
+ * @author Clinton Begin
+ * @modify muse
+ */
+public interface DataSourceFactory {
+
+    void setProperties(Properties props);
+
+    DataSource getDataSource();
+
+}
